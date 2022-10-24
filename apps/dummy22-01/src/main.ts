88c90b6aa0aa3294b8504dc6400ae4f58d3e55c2
githubdmn <dmninbox@gmail.com>
2022-10-24 17:17:48 +0200

    global validator setup

--- apps/dummy22-01/src/main.ts
+++ apps/dummy22-01/src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { Dummy2201Module } from "./dummy22-01.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(Dummy2201Module);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(8022);
 }
 bootstrap();
