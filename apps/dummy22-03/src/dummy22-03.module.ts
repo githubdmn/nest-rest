c8cc80db22d51af866575b997e5f8d58ebab6ce3
githubdmn <dmninbox@gmail.com>
2023-02-09 10:21:12 +0100

    main - validation pipe

--- apps/dummy22-03/src/dummy22-03.module.ts
+++ apps/dummy22-03/src/dummy22-03.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { Dummy2203Controller } from './dummy22-03.controller';
-import { Dummy2203Service } from './dummy22-03.service';
 
 @Module({
   imports: [],
-  controllers: [Dummy2203Controller],
-  providers: [Dummy2203Service],
+  controllers: [],
+  providers: [],
 })
 export class Dummy2203Module {}
--- apps/dummy22-03/src/main.ts
+++ apps/dummy22-03/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { env } from './conf';
 import { Dummy2203Module } from './dummy22-03.module';
 
 async function bootstrap() {
+  console.log(`\n\n\n\nDummy22-03  listening on port: ${env.port}`);
   const app = await NestFactory.create(Dummy2203Module);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.listen(env.port);
 }
 bootstrap();
