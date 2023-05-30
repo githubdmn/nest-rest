4142f2f75e76971cf17d04945f3d9bda7a681295
githubdmn <dmninbox@gmail.com>
2023-05-30 20:56:17 +0200

    createCard

--- apps/dummy22-03/src/card/card.service.ts
+++ apps/dummy22-03/src/card/card.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CardEntity } from '../entities';
 import { Repository } from 'typeorm';
+import { CreateCardDto } from './dto';
 
 @Injectable()
 export class CardService {
@@ -10,9 +11,19 @@ export class CardService {
     private cardRepository: Repository<CardEntity>,
   ) {}
 
-  createCard() {}
-  getCard() {}
-  getCards() {}
-  updateCard() {}
-  deleteCard() {}
+  async createCard(card: CreateCardDto) {
+    const created = await this.cardRepository.create(card);
+    return await this.cardRepository.save(created);
+  }
+
+  async getCard(id: string) {
+    return await this.cardRepository.findOneBy({ cardId: id });
+  }
+
+  // async getCards(user_id: string) {}
+
+  // //TODO: update card DTO
+  // async updateCard(id: string) {}
+
+  // async deleteCard(id: string) {}
 }
