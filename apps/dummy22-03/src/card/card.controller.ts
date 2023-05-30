b14ab5818b04198b65d5fea3b1f56c396ed7e06c
githubdmn <dmninbox@gmail.com>
2023-05-30 20:55:55 +0200

    TODO: finish getCard

--- apps/dummy22-03/src/card/card.controller.ts
+++ apps/dummy22-03/src/card/card.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { CardService } from './card.service';
+import { CreateCardDto } from './dto';
 
 @Controller('card')
-export class CardController {}
+export class CardController {
+  constructor(private card: CardService) {}
+
+  @Post('')
+  async createCard(@Body() body: CreateCardDto) {
+    return await this.card.createCard(body);
+  }
+
+  @Get('')
+  async getCard(@Param('id') id: string) {
+    return await this.card.getCard(id);
+  }
+}
