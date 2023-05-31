13fdc3e86ec9b12f205605475b563a411b2e4e20
githubdmn <dmninbox@gmail.com>
2023-05-31 14:25:40 +0200

    added get update delete

--- apps/dummy22-03/src/card/card.service.ts
+++ apps/dummy22-03/src/card/card.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CardEntity } from '../entities';
 import { Repository } from 'typeorm';
-import { CreateCardDto } from './dto';
+import { CreateCardDto, UpdateCardDto } from './dto';
 
 @Injectable()
 export class CardService {
@@ -20,10 +20,35 @@ export class CardService {
     return await this.cardRepository.findOneBy({ cardId: id });
   }
 
-  // async getCards(user_id: string) {}
+  async getCards(userId: string) {
+    const cards = await this.cardRepository.find({
+      where: {
+        userId: userId,
+      },
+    });
+    return cards.map((card) => ({ ...card, userId: userId }));
+  }
 
-  // //TODO: update card DTO
-  // async updateCard(id: string) {}
+  async updateCard(card: UpdateCardDto) {
+    return await this.cardRepository.update(card.cardId, card);
+  }
 
-  // async deleteCard(id: string) {}
+  async updatePartialCard(card: UpdateCardDto) {
+    return await this.cardRepository.update(card.cardId, card);
+  }
+
+  async deleteCard(cardId: string) {
+    const card = await this.getCard(cardId);
+    return await this.cardRepository.remove(card);
+    // return await this.cardRepository.delete({ cardId: cardId });
+  }
+
+  async deleteCards(userId: string) {
+    const cards = await this.getCards(userId);
+    const results = [];
+    for (const card of cards) {
+      results.push(await this.cardRepository.delete({ cardId: card.cardId }));
+    }
+    return results; //await this.cardRepository.delete({ userId: userId });
+  }
 }
